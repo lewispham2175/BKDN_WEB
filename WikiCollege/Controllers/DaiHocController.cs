57f86d52b5233ed06e8ffbcc2f58236f5b1a291d
Lewis Pham <Lewis Pham@DESKTOP-K9U0O2H>
2020-06-29 00:49:55 +0700

    DaiHoc

---
+++ WikiCollege/Controllers/DaiHocController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WikiCollege.Controllers
+{
+    public class DaiHocController : Controller
+    {
+        // GET: DaiHoc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WikiCollege/Models/Dao/UserDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WikiCollege.Models.EF;
+
+namespace WikiCollege.Models.Dao
+{
+    public class UserDao
+    {
+        WikiCollegeDBContext db = new WikiCollegeDBContext();
+
+    }
+}
